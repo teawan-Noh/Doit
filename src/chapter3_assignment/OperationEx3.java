3b0492420595262eac0f395d9b6eb6e3794c3d17
나 <@192.168.1.172>
2021-06-06 11:20:12 +0900

    210606

--- src/chapter3_assignment/OperationEx3.java
+++ src/chapter3_assignment/OperationEx3.java
@@ -10,7 +10,7 @@ public class OperationEx3 {
 		int num1 = 10;
 		int i = 2;
 		
-		boolean value = ( ((num1 = num1 + 10) < 10) &&((i = i +2) > 10) );
+		boolean value = ( ((num1 = num1 + 10) < 10) && ((i = i +2) > 10) );
 		System.out.println(value); //false
 		System.out.println(num1);  //20
 		System.out.println(i);     //2  앞의 항이 false 이므로 뒷 항은 실행되지 않음
